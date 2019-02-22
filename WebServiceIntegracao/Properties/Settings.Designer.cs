a96d58b67fb81b6c2fc7a27e2c955dfdb1f0c44d
Rodolfo Cortese <rodolfo.cortese@easyepi.com.br>
2019-02-22 18:39:19 -0300

    WBSIntegracao: removida WebReference "InsertTeste"
    Na verdade era uma referência a outra versão do mesmo WebService.
    Não foi testado ainda

--- WebServiceIntegracao/Properties/Settings.Designer.cs
+++ WebServiceIntegracao/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace WebServiceIntegracao.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,16 +20,6 @@ namespace WebServiceIntegracao.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://easyepi.com.br/Teste/Client.asmx")]
-        public string WebServiceIntegracao_InsertTeste_Client {
-            get {
-                return ((string)(this["WebServiceIntegracao_InsertTeste_Client"]));
             }
         }
     }
